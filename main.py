c36269df0f76212ccec0c27e1d6ea862f33d7116
Shit And Giggles <dtran2002@ucr-secure-41-10-13-214-7.wnet.ucr.edu>
2022-11-29 13:10:57 -0800

    This is B, Mergsorting

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import numpy as np
 # @arg arr The input list like object to be sorted
 # @arg cmp A compare function which takes two element in the array, 
 #          cmp(a,b)<0   if a should be placed before b,
@@ -41,8 +42,38 @@ def merge_sort(arr,cmp):
             arr[k] = R[j]
             j += 1
             k += 1
-    pass
+    return arr
 
 # must be in-place sort
+<<<<<<< HEAD
 def quick_sort(arr,cmp):
     pass
+=======
+def quick_sort(arr, cmp):
+    if len(arr)<=1: 
+        return arr
+    pvt=arr[np.random.randint(len(arr))]   # Picks random element as pivot
+    arrlarge,arrsmall,arrequal=[],[],[]
+    for e in arr:
+        if cmp(e, pvt) == 1:
+            arrlarge.append(e)
+        elif cmp(e,pvt) == -1:
+            arrsmall.append(e)
+        else:
+            arrequal.append(e)
+
+    # Call recursion on left and right
+    arrsmall=quick_sort(arrsmall)
+    arrlarge=quick_sort(arrlarge)
+    arr=arrsmall+arrequal+arrlarge
+    return arr
+
+
+# def cmp(a,b):
+#     if(a<b):
+#         return -1
+#     elif(a==b):
+#         return 0
+#     else:
+#         return 1
+>>>>>>> d06dc43138c12b1e00ea57f96046557aef1e6d9b
